33738ed8915828836555388997debbae67ce4ba2
Vijayrajvijay <vr104555@gmail.com>
2023-12-15 18:50:26 +0530

    upadate router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ import userRoutes from './userRoutes.js'
 
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    res.status(200).send(` <h1> Welcome to Diary Manager backend</h1>`);
+  });
+
 router.use('/user',userRoutes)
 
 export default router
